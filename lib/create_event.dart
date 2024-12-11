43ef2d4c061725d1238f8894e16ecb2ee44d29f0
cheesyGarlicBread15 <160604792+cheesyGarlicBread15@users.noreply.github.com>
2024-12-12 06:54:08 +0800

    implemented firebase add, added create event mark location (fab visibility only)

--- lib/create_event.dart
+++ lib/create_event.dart
@@ -1,21 +1,34 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class CreateEvent extends StatelessWidget {
-  CreateEvent({super.key});
+  final VoidCallback onCoordMark;
+  CreateEvent({super.key, required this.onCoordMark});
 
+  final dbRef = FirebaseDatabase.instance.ref("events");
   final nameController = TextEditingController();
   final descriptionController = TextEditingController();
+  final coordController = TextEditingController();
 
-  void _create() {
-    print(nameController.text);
-    print(descriptionController.text);
+  void _create(BuildContext context) {
+    final id = DateTime.now().millisecondsSinceEpoch.toString();
+    dbRef.child(id).set(
+      {
+      'id' : id,
+      'name' : nameController.text.toString(),
+      'description' : descriptionController.text.toString(),
+      'coordinates' : coordController.text.toString()
+      });
+    nameController.clear();
+    descriptionController.clear();
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(10, 80, 10, 10),
-      child: Container(
+      child: SizedBox(
         width: double.infinity,
         child: Column(
           children: [
@@ -45,6 +58,29 @@ class CreateEvent extends StatelessWidget {
               ),
               controller: descriptionController,
             ),
+            const SizedBox(height: 20),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Coordinates',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2
+                  ),
+                )
+              ),
+              controller: coordController,
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    onCoordMark();
+                  },
+                  child: const Text('Mark'),
+                ),
+              ],
+            ),
             Container(
               padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
               child: Row(
@@ -58,7 +94,7 @@ class CreateEvent extends StatelessWidget {
                   ),
                   const SizedBox(width: 20),
                   ElevatedButton(
-                    onPressed: _create,
+                    onPressed: () => _create(context),
                     child: const Text('Create'),
                   ),
                 ],
--- lib/google_map_page.dart
+++ lib/google_map_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:plan_it/create_event.dart';
@@ -5,12 +6,14 @@ import 'package:plan_it/create_event.dart';
 class GoogleMapPage extends StatefulWidget {
   const GoogleMapPage({super.key});
 
+
   @override
   State<GoogleMapPage> createState() => _GoogleMapPageState();
 }
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
   static const initPos = LatLng(37.4223, -122.0848);
+  bool _showFab = true;
 
   void _createEvent() {
     showModalBottomSheet(
@@ -18,7 +21,14 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
       isDismissible: false,
       enableDrag: false,
       context: context, 
-      builder: (ctx) => CreateEvent());
+      builder: (ctx) => CreateEvent(
+        onCoordMark: () {
+          setState(() {
+            _showFab = false;
+          });
+          Navigator.pop(context);
+        },
+      ));
   }
 
   @override
@@ -37,10 +47,11 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
           )
         },
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: _showFab ? FloatingActionButton(
         onPressed: _createEvent,
         child: const Icon(Icons.add),
-      ),
+      )
+      : null,
     );
   }
 }
